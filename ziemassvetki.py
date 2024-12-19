6c47be9e41189e0bffa2c5ce3b047f53ce3a147d
JazepsA <jazepsaugustans26@gmail.com>
2024-12-19 09:34:02 +0200

    dsa

--- ziemassvetki.py
+++ ziemassvetki.py
@@ -8,11 +8,11 @@ root=Tk()
 
 root.title("Laime")
 root.geometry('1920x1080')
-root.resizable(width=False,height=False)
+root.resizable(width=True,height=True)
+root.configure(background='lightgreen')
 
 
 myFont = font.Font(family='Courier', size=30, weight='bold')
-
 
 
 
@@ -48,34 +48,57 @@ def Whyknb():
     value=choice(knb)
     lableText.configure(text=value)
 
+def rinda():
+    with open("info.txt","r",encoding="utf-8") as file:
+            lines=file.readlines()
+            if lines:
+                rand=random.choice(lines[:][3:]).strip()
+                lable1.configure(text=f"Prognoze uz 2025.gadu {rand}")
+            else:
+                lable1.configure(text="Fails ir tukšs")
+
+
+lable1=Label(root,
+              text='',
+              font=('Comic Sans MS ',20,'bold'),
+              bg='white',
+              fg='blue',
+              )
+lable1.place(x=300,y=900)
+lable1['font']= myFont
+
+label1=Label(root,bg='white',fg='black',)
+label1.pack()
+
 lableText=Label(root,
               text='',
               font=('Comic Sans MS ',20,'bold'),
               #bg='',
               fg='blue',
               )
-lableText.place(x=600,y=550)
+lableText.place(x=600,y=575)
 
 
-btn=Button(root,text='Krāsa',bg='white',command=change_color,height=1,width=5)
-btn.place(x=200,y=200)
+btn=Button(root,text='Krāsa',bg='white',command=change_color,height=1,width=10)
+btn.place(x=200,y=150)
 btn['font'] = myFont
 
 btn2=Button(root,text='Novēlējums',bg='white',command=Whyknb,height=1,width=10)
-btn2.place(x=200,y=500)
+btn2.place(x=200,y=350)
 btn2['font'] = myFont
 
 
-btn3=Button(root,text='Vārds',command=get,bg='white',height=1,width=5)
+btn3=Button(root,text='Vārds',command=get,bg='white',height=1,width=10)
 btn3.pack()
-btn3.place(x=200, y=800)
+btn3.place(x=200, y=550)
 btn3['font'] = myFont
 
+btn4=Button(root,text='2025',command=rinda,bg='white',height=1,width=10)
+btn4.pack()
+btn4.place(x=200, y=750)
+btn4['font'] = myFont
 
-label1=Label(root,bg='white',fg='black',)
-label1['font'] = myFont
 
-label1.pack()
 
 
 root.mainloop()
